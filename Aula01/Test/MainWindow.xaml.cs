68a2a27a60daf1c601010b25b1304d4e1e26bd9a
heron-emanuel <heron.santos@academico.ifrn.edu.br>
2022-10-14 17:37:39 -0300

    Adicionar botão

--- Aula01/Test/MainWindow.xaml.cs
+++ Aula01/Test/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Test
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
     }
 }

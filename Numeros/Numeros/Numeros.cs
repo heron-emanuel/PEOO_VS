6790b318be4ef9d808c494e21edaf80a20356d40
heron-emanuel <heron.santos@escolar.ifrn.edu.br>
2022-10-21 17:56:51 -0300

    Add Numeros

---
+++ Numeros/Numeros/Numeros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros
+{
+    class Numero
+    {
+        public int start;
+        public int end;
+
+        public Numero(int s, int e)
+        {
+            start = s;
+            end = e;
+        }
+
+        public int[] Calcular(bool even, bool odd)
+        {
+            return Enumerable.Range(start, end - start).Where(n => { if (even) { return n % 2 == 0; } else { return odd ? n % 2 != 0 : false; } }).ToArray();
+        }
+    }
+}

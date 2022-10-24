b5bfb6a1151e2aa8666ca0674f8e1b39ce8e7257
heron-emanuel <heron.santos@academico.ifrn.edu.br>
2022-10-24 15:22:07 -0300

    Add Abastecimento

---
+++ Abastecimento/Abastecimento/Abastecer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abastecimento
+{
+    class Abastecer
+    {
+        private double LValue;
+        private double paidVal;
+        private double disTraveled;
+
+        public Abastecer(double lv, double pv, double td)
+        {
+            LValue = lv;
+            paidVal = pv;
+            disTraveled = td;
+        }
+
+        public double MediaKmLitro()
+        {
+            return disTraveled / (paidVal / LValue);
+        }
+
+        public double MediaReaisKm()
+        {
+            return paidVal / disTraveled;
+        }
+    }
+}
---
+++ Abastecimento/Abastecimento/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abastecimento
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecer a = new Abastecer(double.Parse(lValue.Text), double.Parse(paidValue.Text), double.Parse(distance.Text));
+            kmPerL.Text = a.MediaKmLitro().ToString("0.00");
+            valuePerL.Text = a.MediaReaisKm().ToString("0.00");
+        }
+    }
+}
